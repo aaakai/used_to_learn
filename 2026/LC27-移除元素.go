ec4f725753c247fcbf7c9438ce6e9c2489d3c26f
sunjingkai01 <sunjingkai01@baidu.com>
2026-02-09 18:54:58 +0800

    leetcode 27 88

--- 2026/LC27-移除元素.go
+++ 2026/LC27-移除元素.go
@@ -1,5 +1,15 @@
 package _026
 
 func removeElement(nums []int, val int) int {
-
+	if len(nums) <= 1 {
+		return len(nums)
+	}
+	left := 0
+	for right := 0; right < len(nums); right++ {
+		if nums[right] != val {
+			nums[left] = nums[right]
+			left++
+		}
+	}
+	return left
 }
