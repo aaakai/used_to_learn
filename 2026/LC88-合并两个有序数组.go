f2d9d39b1d5a725bb1ea214c3037c8676cdc8002
JackSun <944982629@qq.com>
2026-02-09 21:14:30 +0800

    leetcode 88

--- 2026/LC88-合并两个有序数组.go
+++ 2026/LC88-合并两个有序数组.go
@@ -1,5 +1,25 @@
 package _026
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-
+	n1Tag := m - 1
+	n2Tag := n - 1
+	index := m + n - 1
+	for index >= 0 {
+		res := 0
+		if n1Tag < 0 {
+			res = nums2[n2Tag]
+			n2Tag--
+		} else if n2Tag < 0 {
+			res = nums1[n1Tag]
+			n1Tag--
+		} else if nums1[n1Tag] > nums2[n2Tag] {
+			res = nums1[n1Tag]
+			n1Tag--
+		} else {
+			res = nums2[n2Tag]
+			n2Tag--
+		}
+		nums1[index] = res
+		index--
+	}
 }
