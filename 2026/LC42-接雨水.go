e4e866e4a2362b994cd1accc4653e1c30c002756
sunjingkai01 <sunjingkai01@baidu.com>
2026-02-11 18:42:31 +0800

    leetcode 42

--- 2026/LC42-接雨水.go
+++ 2026/LC42-接雨水.go
@@ -1,1 +1,26 @@
 package _026
+
+func trap(height []int) int {
+	res := 0
+	left, right := 0, len(height)-1
+	lMax, rMax := 0, 0
+	for left < right {
+		lMax = maxI(lMax, height[left])
+		rMax = maxI(rMax, height[right])
+		if height[left] < height[right] {
+			res += lMax - height[left]
+			left++
+		} else {
+			res += rMax - height[right]
+			right--
+		}
+	}
+	return res
+}
+
+func maxI(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
