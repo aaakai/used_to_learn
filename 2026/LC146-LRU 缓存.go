99c6d3144314cf12fc82a91e6336894919997ed9
sunjingkai01 <sunjingkai01@baidu.com>
2026-03-04 19:24:23 +0800

    leetcode

---
+++ 2026/LC146-LRU 缓存.go
@@ -0,0 +1,16 @@
+package _026
+
+type LRUCache struct {
+}
+
+func Constructor(capacity int) LRUCache {
+
+}
+
+func (this *LRUCache) Get(key int) int {
+
+}
+
+func (this *LRUCache) Put(key int, value int) {
+
+}
